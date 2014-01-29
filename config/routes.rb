3938e806eb9f9480d41ad6bef4ada58f1ac8a40d
Prelang Builder <builder@prelang.com>
2014-01-29 16:26:55 +0000

    Generated Devise's User model (via Devise's generator)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Anglr::Application.routes.draw do
+  devise_for :users
   get "landings/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
