3491907735743373e2120365f9222ef054c85223
Prelang Builder <builder@prelang.com>
2014-01-29 16:27:20 +0000

    Generated scaffolding for model via Rails

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :body, :title, :source_url, :slug, :author_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Anglr::Application.routes.draw do
+  resources :articles
+
   resources :authors
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
